816c6545216ab742010c266bef203c71f4c46796
usmanubaid396 <usmanubaid396@gmail.com>
2026-08-14 19:05:16 +0500

    Create blogData.js with initial blog entries
    Added initial blog data with titles and content for two blogs.

---
+++ src/data/blogData.js
@@ -0,0 +1,11 @@
+export const blogs = {
+  'nust-net-2026-series-4-registration-extended': {
+    title: "NUST NET 2026 Series 4 Registration Extended",
+    content: `The National University of Sciences and Technology (NUST) has officially extended the registration deadline for NET-2026 Series 4...` 
+  },
+  'nust-net-2026-merit-list-predictions-engineering': {
+    title: "NUST NET 2026 Merit List Predictions",
+    content: `Every year, thousands of students obsessively refresh the NUST merit list page...`
+  },
+  // Add remaining 3 blogs here following the same pattern
+};
